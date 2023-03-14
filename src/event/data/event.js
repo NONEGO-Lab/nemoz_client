7a1f6ecc4066b3a88e86b3f6acfe55209ccf6e24
NONEGO-Lab <dev@nonegolab.com>
2023-03-14 18:49:22 +0900

    :construction: default props 작업 진행 중

--- src/event/data/event.js
+++ src/event/data/event.js
@@ -1,1 +1,58 @@
-// api 처리하는 놈
+import {instance} from "../../shared/apiConfig";
+
+
+export const eventApi = {
+  getEventList: async ({page, eventId}) => {
+    const data = await instance.get("/event/list", { params:
+          { page_size: 20, current_page: page, event_id: eventId }});
+
+    return {
+      events: data.data.events
+    };
+  },
+
+
+  getEventDetail: async ({page, eventId}) => {
+    const data = await instance.get("/event/list", { params: {
+        page: page, event_id: eventId
+      }});
+    return data.data;
+  },
+
+  createEvent: async ({ name, date, fanIds, staffIds, artistIds, creator }) => {
+    let req = {
+      event_name: name,
+      target_artist_ids: artistIds,
+      target_staff_ids: staffIds,
+      target_fan_ids: fanIds,
+      creator: 1,
+      due_dt: date
+    };
+    const data = await instance.post("/event/register", req);
+    const eventData = data.data;
+    return {
+      id: eventData.id,
+      eventName: eventData.event_name,
+      artistIds: [...eventData.target_artist_ids],
+      staffIds: [...eventData.target.staff_ids],
+      fanIds: [...eventData.target_fan_ids]
+    };
+  },
+
+  updateEvent: async ({id, eventName, artistIds, staffIds, fanIds, creator, dueDt}) => {
+    let req = {
+      id: id,
+      event_name: eventName,
+      target_artist_ids: artistIds,
+      target_staff_ids: staffIds,
+      target_fan_ids: fanIds,
+      creator: creator,
+      due_dt: dueDt
+    };
+    const data = await instance.post("/event/update", req);
+    return data.data;
+  }
+
+
+
+}
---
+++ src/shared/apiConfig.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export const instance = axios.create({
+  baseURL: process.env.REACT_APP_NEMOZ_API_URL,
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+    "Access-Control-Allow-Headers": "*",
+  },
+})
+
+
+instance.interceptors.request.use(function (config) {
+  const accessToken = localStorage.getItem("auth");
+  if(accessToken) {
+    config.headers["Authorization"] = `Bearer ${accessToken}`;
+    return config;
+  }
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+
+instance.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  if(error.response.status === 401) {
+    console.log("response 401 error");
+  }
+  return Promise.reject(error);
+});
