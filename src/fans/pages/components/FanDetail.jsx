7dfb11a75974682641fcf08d43b28f681e4b7f07
Dino Sim <dino@nonegolab.com>
2023-11-14 12:38:21 +0900

    팬 상세정보 스타일 변경

--- src/fans/pages/components/FanDetail.jsx
+++ src/fans/pages/components/FanDetail.jsx
@@ -5,7 +5,7 @@ import {attendeeApi} from "../../data/attendee_data";
 
 const FanDetail = ({setOnModal, currentFanId, eventId}) => {
 
-    let style = "w-[650px] h-[900px] rounded-[15px]  drop-shadow-md";
+    let style = "w-[488px] h-[675px] rounded-[15px] drop-shadow-md";
 
     const [fanInfo, setFanInfo] = useState({});
 
@@ -21,48 +21,48 @@ const FanDetail = ({setOnModal, currentFanId, eventId}) => {
     return (
         <ModalFrameDepth setOnModal={setOnModal} style={style}>
             <div>
-                <div className="text-[24px] font-[500] px-[60px] pt-[60px] pb-[40px]">
+                <div className="text-[1.2rem] font-[500] px-[45px] pt-[45px] pb-[30px]">
                     Fan Info
                 </div>
                 <div>
 
                     <div className={"bg-[#f0f0f0] min-h-[63px] flex items-center"}>
-                        <div className={"text-[19px] min-w-[50px] text-[#444] ml-[77px] mr-[74px]"}>
+                        <div className={"text-[1.2rem] min-w-[50px] text-[#444] mx-[60px]"}>
                             이름
                         </div>
-                        <div className={"text-[21px] text-[#444] font-[500]"}>
+                        <div className={"text-[1.3rem] text-[#444] font-[500]"}>
                             {fanInfo.fan_name || '정보 없음'}
                         </div>
                     </div>
 
                     <div className={"bg-[#fff] min-h-[63px] flex items-center"}>
-                        <div className={"text-[19px] min-w-[50px] text-[#444] ml-[77px] mr-[74px]"}>
+                        <div className={"text-[1.2rem] min-w-[50px] text-[#444] mx-[60px]"}>
                             성별
                         </div>
-                        <div className={"text-[21px] text-[#444] font-[500]"}>
+                        <div className={"text-[1.3rem] text-[#444] font-[500]"}>
                             {fanInfo.sex || '정보 없음'}
                         </div>
                     </div>
 
                     <div className={"bg-[#f0f0f0] min-h-[63px] flex items-center"}>
-                        <div className={"text-[19px] min-w-[50px] text-[#444] ml-[77px] mr-[74px]"}>
+                        <div className={"text-[1.2rem] min-w-[50px] text-[#444] mx-[60px]"}>
                             나이
                         </div>
-                        <div className={"text-[21px] text-[#444] font-[500]"}>
+                        <div className={"text-[1.3rem] text-[#444] font-[500]"}>
                             {fanInfo?.age || '나이 없음'}
                         </div>
 
                     </div>
 
                     <div className={"bg-[#fff] min-h-[63px] flex items-center"}>
-                        <div className={"text-[19px] min-w-[50px] text-[#444] ml-[77px] mr-[74px]"}>
+                        <div className={"text-[1.2rem] min-w-[50px] text-[#444] mx-[60px]"}>
                             팬레터
                         </div>
                     </div>
 
                     <div className={"bg-[#f0f0f0] min-h-[350px] flex"}>
                         <div
-                            className={"text-[21px] font-[500] min-w-[50px] text-[#444] ml-[77px] mr-[74px] mt-[36px]"}>
+                            className={"text-[1.3rem] font-[500] min-w-[50px] text-[#444] mx-[60px] mt-[36px]"}>
                             {fanInfo?.letter}
                         </div>
                     </div>
@@ -70,11 +70,11 @@ const FanDetail = ({setOnModal, currentFanId, eventId}) => {
                 </div>
                 <Button
                     style={"fixed bottom-[60px] right-[60px] rounded-[10px] border-[1px] border-[#aaa] flex items-center justify-center"}
-                    width={"w-[140px]"}
+                    width={"w-[106px]"}
                     textColor={"text-[#444]"}
                     _onClick={setOnModal}>
                   <span className={"my-[18px]"}>
-               <img src={"../images/closeIcon.png"} className={"w-[17px] h-[17px]"} alt={"plus-icon"}/>
+               <img src={"/images/closeIcon.png"} className={"w-[15px] h-[15px]"} alt={"close-icon"}/>
              </span>
                     <span className={"ml-[8px] text-[16px] text-[#444]"}>Close</span>
                 </Button>
